04ae1f024e4e4b586bad3f8a0fee89bf2c5905c9
Sqikys <kaustavmahata@gmail.com>
2025-12-08 21:13:52 +0530

    added edit, bulk approve, delete endpoints

--- backend/routes/text_guide.route.js
+++ backend/routes/text_guide.route.js
@@ -1,6 +1,18 @@
 import express from "express";
-import { approve_or_reject_rejection_request, approve_or_reject_text, get_all_guides, get_text_guide, get_text_guide_by_id, post_text_guide,approve_or_reject_for_support } from "../controllers/text_guide.controller.js";
-import { adminRoute, protectRoute, supportOnlyRoute, supportOrAdminRoute } from "../middleware/auth.middleware.js";
+import { approve_or_reject_rejection_request,
+        approve_or_reject_text, get_all_guides,
+        get_text_guide,
+        get_text_guide_by_id,
+        post_text_guide,
+        approve_or_reject_for_support,
+        bulk_action_text_guides,
+        update_text_guide,
+        delete_text_guide} from "../controllers/text_guide.controller.js";
+import { adminRoute,
+        protectRoute,
+        supportOnlyRoute, 
+        supportOrAdminRoute,
+        canEditGuideMiddleware} from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
@@ -17,5 +29,24 @@ router.put("/approve_or_deny_rejection_request/:id",protectRoute, adminRoute, ap
 
 //support view
 router.put("/approve_or_reject_text_guide/:id",protectRoute, supportOnlyRoute, approve_or_reject_for_support)
+
+router.post(
+  "/text_guides/bulk_action",
+  protectRoute,
+  adminRoute,
+  bulk_action_text_guides
+);
+router.put(
+  "/text_guide/:id",
+  protectRoute,
+  canEditGuideMiddleware, // admin OR owner
+  update_text_guide
+);
+router.delete(
+  "/text_guide/:id",
+  protectRoute,
+  adminRoute,
+  delete_text_guide
+);
 
 export default router;
