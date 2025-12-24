908422784f7e2c6e13df6e215e952af8a97a19a0
Shreedeep Datta <shreedeepdatta2@gmail.com>
2025-12-24 12:21:42 +0530

    Backend url removed

--- frontend/src/api/modules/community_forum.ts
+++ frontend/src/api/modules/community_forum.ts
@@ -2,63 +2,61 @@ import { CommunityApproveRejectDeleteResponse, CommunityForumDetailResponse, Com
 import httpClient from "../axiosSetup";
 
 
-const BACKEND_URL = import.meta.env.VITE_BASE_URL_DEV
-
 export const community_forum_api = {
     create: async(data: CommunityForumPayload) =>{
-        const res = await httpClient.post<CommunityForumDetailResponse>(`${BACKEND_URL}/api/community/add_community_forum`, data, {
+        const res = await httpClient.post<CommunityForumDetailResponse>(`/api/community/add_community_forum`, data, {
             headers: { useAuth: true },
         });
         return res.data;
     },
 
     getAllCommunityForums: async (page: number) => {
-        const res = await httpClient.get<getAllCommunityForum>(`${BACKEND_URL}/api/community/get_all_community_forum?page=${page}`, {
+        const res = await httpClient.get<getAllCommunityForum>(`/api/community/get_all_community_forum?page=${page}`, {
             headers: {useAuth: true},
         });
         return res.data;
     },
 
     getAllApprovedCommunityForums: async (page: number): Promise<getAllCommunityForum> => {
-        const res = await httpClient.get<getAllCommunityForum>(`${BACKEND_URL}/api/community/get_all_approved_community_forum?page=${page}`);
+        const res = await httpClient.get<getAllCommunityForum>(`/api/community/get_all_approved_community_forum?page=${page}`);
         return res.data;
     },
 
     getCommunityforumByid: async (id: string)=>{
-        const res = await httpClient.get<CommunityForumDetailResponse>(`${BACKEND_URL}/api/community/get_community_forum_by_id/${id}`);
+        const res = await httpClient.get<CommunityForumDetailResponse>(`/api/community/get_community_forum_by_id/${id}`);
         return res.data;
     },
 
     approveCommunity: async (forum_id: string)=>{
-        const res = await httpClient.put<CommunityApproveRejectDeleteResponse>(`${BACKEND_URL}/api/community/approve_community?forum_id=${forum_id}`,{
+        const res = await httpClient.put<CommunityApproveRejectDeleteResponse>(`/api/community/approve_community?forum_id=${forum_id}`,{
             headers: {useAuth: true},
         });
         return res.data;
     },
 
     rejectCommunity: async (forum_id: string)=>{
-        const res = await httpClient.put<CommunityApproveRejectDeleteResponse>(`${BACKEND_URL}/api/community/reject_community?forum_id=${forum_id}`,{
+        const res = await httpClient.put<CommunityApproveRejectDeleteResponse>(`/api/community/reject_community?forum_id=${forum_id}`,{
             headers: {useAuth: true},
         });
         return res.data;
     },
 
     approveRejectionRequest: async (forum_id: string)=>{
-        const res = await httpClient.put<CommunityApproveRejectDeleteResponse>(`${BACKEND_URL}/api/community/approve_rejection_request?forum_id=${forum_id}`,{
+        const res = await httpClient.put<CommunityApproveRejectDeleteResponse>(`/api/community/approve_rejection_request?forum_id=${forum_id}`,{
             headers: {useAuth: true},
         });
         return res.data;
     },
 
     deleteCommunityForum: async (forum_id: string)=>{
-        const res = await httpClient.delete<CommunityApproveRejectDeleteResponse>(`${BACKEND_URL}/api/community/delete_community_forum?forum_id=${forum_id}`,{
+        const res = await httpClient.delete<CommunityApproveRejectDeleteResponse>(`/api/community/delete_community_forum?forum_id=${forum_id}`,{
             headers: {useAuth: true},
         });
         return res.data;
     },
 
     deleteComment: async (comment_id: string)=>{
-        const res = await httpClient.delete<CommunityApproveRejectDeleteResponse>(`${BACKEND_URL}/api/community/comment?comment_id=${comment_id}`, {
+        const res = await httpClient.delete<CommunityApproveRejectDeleteResponse>(`/api/community/comment?comment_id=${comment_id}`, {
             headers: {useAuth: true},
         });
         return res.data;
