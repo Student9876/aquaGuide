3d8322efe6eb6a5e0b282417fdb22f51c0d77967
Sayantansen123 <riosayan123@gmail.com>
2026-01-07 12:26:14 +0530

    communioty chat api

--- backend/routes/community_chat.route.js
+++ backend/routes/community_chat.route.js
@@ -1,11 +1,16 @@
 import express from "express";
 import {
-    getAllMessages,
-    getRecentMessages,
-    getUserMessages,
-    getChatStatistics,
+  getAllMessages,
+  getRecentMessages,
+  getUserMessages,
+  getChatStatistics,
+  createCommunity,
 } from "../controllers/community_chat.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  adminRoute,
+  protectRoute,
+  supportOrAdminRoute,
+} from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -21,7 +26,7 @@ router.get("/stats", getChatStatistics);
 
 // Protected routes
 router.get("/", protectRoute, getAllMessages);
+router.post("/createcommunity", supportOrAdminRoute, createCommunity);
 router.get("/:userId", protectRoute, getUserMessages);
 
 export default router;
-
