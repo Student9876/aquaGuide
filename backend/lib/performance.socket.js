491e42250aedc48ae8007a1c2b97c768b8bf1ece
Sqikys <kaustavmahata@gmail.com>
2026-01-12 22:45:53 +0530

    server performance done, added middleware for sockets

--- backend/lib/performance.socket.js
+++ backend/lib/performance.socket.js
@@ -1,7 +1,10 @@
 import { performanceMonitor } from "../services/performance.monitor.js";
+import { socketAuthMiddleware,socketAdminOrSupportMiddleware } from "../middleware/auth.middleware.js";
 
 export const setupPerformanceSocket = (io) => {
   const nsp = io.of("/performance");
+  nsp.use(socketAuthMiddleware);
+  nsp.use(socketAdminOrSupportMiddleware);
 
   nsp.on("connection", (socket) => {
     console.log("[Performance WS] connected:", socket.id);
--- backend/routes/performance.route.js
+++ backend/routes/performance.route.js
@@ -4,11 +4,11 @@ import {
   getHistory,
   getSummary,
 } from "../controllers/performance.controller.js";
-
+import { protectRoute, supportOrAdminRoute } from "../middleware/auth.middleware.js";
 const router = express.Router();
 
-router.get("/metrics", getMetrics);
-router.get("/history", getHistory);
-router.get("/summary", getSummary);
+router.get("/metrics", protectRoute , supportOrAdminRoute, getMetrics);
+router.get("/history", protectRoute , supportOrAdminRoute, getHistory);
+router.get("/summary", protectRoute , supportOrAdminRoute, getSummary);
 
 export default router;
